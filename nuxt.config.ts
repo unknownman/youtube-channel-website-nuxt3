c21865a80fa0090fc47c26f9f7983db49449cd65
Ali Joder <ali.masoudi.alavi@gmail.com>
2025-03-30 19:46:10 +0330

    activate tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  css: [
+    "~/assets/css/main.css"
+  ],
   modules: [
     '@nuxt/fonts',
     '@nuxt/icon',
