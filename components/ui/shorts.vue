5a85bba9379810d1cfea08a6a3741f8cacedd4b0
Ali Joder <ali.masoudi.alavi@gmail.com>
2025-04-16 11:11:02 +0330

    improve localization

--- components/ui/shorts.vue
+++ components/ui/shorts.vue
@@ -1,12 +1,11 @@
 <template>
-    <section
-        class="flex flex-col rounded-md bg-gradient-to-br from-stone-50 to-stone-200 p-6 shadow-inner inset-shadow-2xs inset-shadow-zinc-200">
-        <h2 class="mb-4 text-xl font-semibold text-stone-800">{{t("shorts")}}</h2>
+    <section class="flex flex-col rounded-md bg-gradient-to-br from-stone-50 to-stone-200 p-6 shadow-inner inset-shadow-2xs inset-shadow-zinc-200">
+        <h2 class="mb-4 text-xl font-semibold text-stone-800">{{ t("shorts") }}</h2>
         <div v-if="loading" class="flex justify-center items-center h-60">
-            <span class="text-stone-500">در حال بارگذاری...</span>
+            <span class="text-stone-500">{{ t('loading') }}...</span>
         </div>
         <div v-else-if="error" class="flex justify-center items-center h-60">
-            <span class="text-red-500">خطا در بارگذاری ویدیوها</span>
+            <span class="text-red-500">{{ t('error.fetch') }}</span>
         </div>
         <div v-else class="-mr-2 h-60 flex-1 space-y-3 overflow-y-auto pr-2 md:h-auto">
             <div v-for="short in latestShorts" :key="short.videoId"
@@ -15,7 +14,7 @@
                     class="mr-3 h-20 w-14 flex-shrink-0 rounded-md object-cover" />
                 <div class="min-w-0">
                     <h3 class="truncate text-sm font-medium text-stone-800">{{ short.title }}</h3>
-                    <p class="text-xs text-stone-500">{{ short.views }} بازدید</p>
+                    <p class="text-xs text-stone-500">{{ short.views }} {{ t('views') }}</p>
                 </div>
             </div>
         </div>
